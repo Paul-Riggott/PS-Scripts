da636eb32c28ac799f8a8fd7430df25975212fa8
Paul-Riggott <paul.riggott@talktalk.net>
2017-07-28 11:33:16 +0100

    Add files via upload

---
+++ Dump XMP.jsx
@@ -0,0 +1,18 @@
+﻿#target bridge;
+if( BridgeTalk.appName == "bridge" ) { 
+DumpXMP = new MenuElement("command", "Dump XMP", "at the end of tools");
+}
+DumpXMP.onSelect = function () { 
+if (ExternalObject.AdobeXMPScript == undefined) ExternalObject.AdobeXMPScript = new ExternalObject("lib:AdobeXMPScript");
+var items = app.document.getSelection("jpg,tif,gif,psd,pcx,png,eps,crw,cr2,tiff,raw,rw2,dng,nef,orf,erf,mos,dcr,raf,srf,pef,x3f");
+for(var a in items){
+var thumb = items[a];
+var f = new File(Folder.desktop +"/" + decodeURI(thumb.spec.name).replace(/\.[^\.]+$/, '') +".txt");
+f.open("w", "TEXT", "????");
+var md =thumb.synchronousMetadata;
+var xmp =  new XMPMeta(md.serialize());
+f.writeln(xmp.dumpObject());
+f.close();
+}
+alert("All done");
+};
---
+++ NameSpace List.jsx
@@ -0,0 +1,13 @@
+﻿#target bridge 
+if( BridgeTalk.appName == "bridge" ) { 
+NameSpace = new MenuElement("command", "NameSpace List", "at the end of tools");
+}
+NameSpace.onSelect = function () { 
+if (ExternalObject.AdobeXMPScript == undefined) ExternalObject.AdobeXMPScript = new ExternalObject("lib:AdobeXMPScript");
+var xmp = new XMPMeta("");
+var f = new File(Folder.desktop +"/Name Spaces.txt");
+f.open("w", "TEXT", "????");
+f.writeln(XMPMeta.dumpNamespaces());
+f.close();
+alert("Name Spaces.txt has been created on the desktop");
+};
